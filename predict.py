bad085a9159c35d04ecea4ccb6ddd4a60e2de14a
Guilherme Augusto <guilherme.francais@gmail.com>
2019-12-07 00:15:21 -0300

    Comments all files needed and improve code comprehension

--- predict.py
+++ predict.py
@@ -1,11 +1,3 @@
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
-from keras.layers import LeakyReLU
-from keras.layers import Dropout
-from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
 from keras import backend as K
 import numpy as np
@@ -13,38 +5,49 @@ import pickle
 from sklearn.datasets import load_files
 from keras.utils import np_utils
 
-filename = 'training_oil_savemodel.sav'
+# Open trained data model to predict Dog image
+filename = 'utils/training_oil_savemodel.sav'
 file = open(filename, 'rb')
 classifier = pickle.load(file)
 
-# load InceptionResNet V2 model
+# Load InceptionResNet V2 model
 from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
 model = InceptionResNetV2(include_top=False, weights='imagenet')
+
+# Save test data using InceptionResNetV2 model
+# Reference: https://github.com/liyenhsu/Dog-Breed-Identification
 from keras.preprocessing import image
 from tqdm import tqdm
+from sklearn.datasets import load_files
+
 def path_to_tensor(img_path):
-    # load RGB image. Use image size (299, 299) for InceptionResNetV2 model
     img = image.load_img(img_path, target_size=(299, 299))
-    # convert image to 3D tensor with shape (299, 299, 3)
     x = image.img_to_array(img)
-    # convert 3D tensor to 4D tensor with shape (1, 299, 299, 3)
     return np.expand_dims(x, axis=0)
+
 def paths_to_tensor(img_paths):
     list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
     return np.vstack(list_of_tensors)
-from sklearn.datasets import load_files
+
 def load_dataset(path, shuffle=True):
     data = load_files(path, shuffle=shuffle)
     files = data['filenames']
-    labels = np_utils.to_categorical(data['target'], 14) # There are 120 dog breeds
+    labels = np_utils.to_categorical(data['target'], 14)
     return files, labels
-test_file, _ = load_dataset('test/', shuffle=False)
+
+test_file, _ = load_dataset('assets/', shuffle=False)
+
 test_tensor = paths_to_tensor(test_file)
+
+# Creates predicted data and saves into file
 features_test = model.predict(preprocess_input(test_tensor))
-np.save(open('features_test.npy', 'wb'), features_test)
-X_test = np.load('features_test.npy')
+np.save(open('utils/features_test.npy', 'wb'), features_test)
+
+# Predict data from model loaded and get index of dog breed
+X_test = np.load('utils/features_test.npy')
 result = classifier.predict(X_test)
 result = result[0]
+
 greater = -1
 value = -1
 i = 0
@@ -56,14 +59,12 @@ for res in result:
         value = i
     i += 1
 
+# Rescue dog breed listing all valid dog breeds of this repository
 import os
-#print([dog for dog in os.walk(os.getcwd() + '/dataset/test_dataset/')])
+
 dogs = [dog[0].split('/')[-1] for dog in os.walk(os.getcwd() + '/dataset/test_dataset/')]
-
-dogs = [dog for dog in dogs if dog != '' or dog != 'test']
-
+dogs = [dog for dog in dogs if dog != '' and dog != 'test']
 dogs.sort()
 
-print(dogs)
 print('Acurácia: ' + str(greater))
 print('Raça: ' + dogs[value])
--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 from sklearn.datasets import load_files
 from keras.utils import np_utils
 import numpy as np
-# from numba import jit, cuda
 
-# @jit(target ="cuda")
+
+dog_breeds = 14
+
 def load_dataset(path, shuffle=True):
     data = load_files(path, shuffle=shuffle)
     files = data['filenames']
-    labels = np_utils.to_categorical(data['target'], 14) # There are 120 dog breeds
+    labels = np_utils.to_categorical(data['target'], dog_breeds)
     return files, labels
 
 # load train and test datasets
@@ -15,13 +16,11 @@ train_files, train_labels = load_dataset('dataset/training_dataset/train')
 test_files, _ = load_dataset('dataset/test_dataset/test', shuffle=False)
 
 # save the labels of the train set
-np.save('train_labels.npy', train_labels)
-
+np.save('utils/train_labels.npy', train_labels)
 
 from keras.preprocessing import image
 from tqdm import tqdm
 
-# @jit(target ="cuda")
 def path_to_tensor(img_path):
     # load RGB image. Use image size (299, 299) for InceptionResNetV2 model
     img = image.load_img(img_path, target_size=(299, 299))
@@ -30,7 +29,6 @@ def path_to_tensor(img_path):
     # convert 3D tensor to 4D tensor with shape (1, 299, 299, 3)
     return np.expand_dims(x, axis=0)
 
-# @jit(target ="cuda")
 def paths_to_tensor(img_paths):
     list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
     return np.vstack(list_of_tensors)
@@ -41,11 +39,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # pre-process the data for Keras
 train_tensors = paths_to_tensor(train_files)
-print('*'*100)
-print(test_files)
-print(train_files)
 test_tensors = paths_to_tensor(test_files)
-
 
 # load InceptionResNet V2 model
 from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
@@ -54,6 +48,6 @@ model = InceptionResNetV2(include_top=False, weights='imagenet')
 # preprocess the tensors and get bottleneck_features
 # These codes take a long time. It's better to use GPU/cloud services
 bottleneck_features_train = model.predict(preprocess_input(train_tensors))
-np.save(open('bottleneck_features_train.npy', 'wb'), bottleneck_features_train)
+np.save(open('utils/bottleneck_features_train.npy', 'wb'), bottleneck_features_train)
 bottleneck_features_test = model.predict(preprocess_input(test_tensors))
-np.save(open('bottleneck_features_test.npy', 'wb'), bottleneck_features_test)
+np.save(open('utils/bottleneck_features_test.npy', 'wb'), bottleneck_features_test)
